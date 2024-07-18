8187d16403e1e5f0664b976d367f4e212bb3180e
Muhammad Umer Farooq <umer@lablnet.com>
2024-07-18 08:53:35 +0500

    refactor: Update data collection schema in config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const data = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		year: z.number(),
+		dimensions: z.string(),
+		s3Url: z.string(),
+		catagory: z.string(),
+		forSale: z.boolean().default(false),
+		draft: z.boolean().default(false),
 	}),
 });
 
-export const collections = { blog };
+export const collections = { 
+	"data": data,
+};
