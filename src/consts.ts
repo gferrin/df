354c6acfde5347675ea5d5cc6e4e75f3ee60b733
Muhammad Umer Farooq <umer@lablnet.com>
2024-07-24 06:35:36 +0500

    refactor: Update menu item names for better readability

--- src/consts.ts
+++ src/consts.ts
@@ -5,9 +5,9 @@ export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 
 export const menuItems = [
-    { id: 'home', name: 'Self Portraits' },
-    { id: 'about', name: 'Figure Paintings' },
-    { id: 'contact', name: 'Still Lifes' },
+    { id: 'self-protraits', name: 'Self Portraits' },
+    { id: 'figure-paintings', name: 'Figure Paintings' },
+    { id: 'still-lifes', name: 'Still Lifes' },
     { id: 'landscapes', name: 'Landscapes' },
     { id: 'drawings', name: 'Drawings' },
     { id: 'murals', name: 'Murals' },
