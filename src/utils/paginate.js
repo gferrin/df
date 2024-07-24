a664d951778dec14eb0b8836334182037efbb248
Muhammad Umer Farooq <umer@lablnet.com>
2024-07-24 09:07:42 +0500

    refactor: Remove unused paginate utility function

--- src/utils/paginate.js
+++
@@ -1,5 +0,0 @@
-export function paginate(items, page, itemsPerPage) {
-	const startIndex = (page - 1) * itemsPerPage;
-	const endIndex = startIndex + itemsPerPage;
-	return items.slice(startIndex, endIndex);
-}
