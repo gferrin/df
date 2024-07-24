6aa9cd43f0057a4ae0f8c99176e7d65883421f08
Muhammad Umer Farooq <umer@lablnet.com>
2024-07-24 09:47:46 +0500

    refactor: Update SITE_TITLE constant to 'DF'

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'DF';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const email = 'hello@douglasferrin.com';
 export const MAX_POST_PER_PAGE = 5;
