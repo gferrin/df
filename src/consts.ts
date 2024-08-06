4daa8de52f62b2183b7480044088d5178964a754
Muhammad Umer Farooq <umer@lablnet.com>
2024-08-06 08:51:39 +0500

    feat: Add press items to press page
    This commit adds press items to the press page. The `pressItems` array in `consts.ts` is updated with new press links, including interviews and articles featuring the artist, Douglas Ferrin. The press items are now dynamically rendered on the press page using the `pressItems` array.
    
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/consts.ts
+++ src/consts.ts
@@ -13,3 +13,10 @@ export const menuItems = [
     { id: 'drawings', name: 'Drawings' },
     { id: 'murals', name: 'Murals' },
 ];
+
+export const pressItems = [
+    { href: 'http://ww16.theartroomonline.net/2013/02/an-interview-with-douglas-ferrin.html?sub1=20240806-1342-217a-ae12-ad8ca28593d8', name: 'Ferrin Artist Interview — The Art Room' },
+    { href: 'http://business.highbeam.com/138173/article-1G1-14212085/developing-artist', name: 'Devolping as an Artist — American Artist Magazine' },
+    { href: 'https://www.theartroomonline.net/2014/03/ugliness-in-art-douglas-ferrin.html', name: 'Strange Beauty: Douglas Ferrin — The Art Room' },
+    { href: 'http://ww16.theartroomonline.net/2014/01/favorites-of-2013-douglas-ferrin.html?sub1=20240806-1343-330b-8217-023e542f2836', name: 'Favorites of 2013: Douglas Ferrin — The Art Room' },
+]
